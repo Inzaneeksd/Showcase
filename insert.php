7429601d9d0aa33bc64aa5ca76409145397bf2d3
Inzaneeksd <17danielmoller@gmail.com>
2021-10-11 02:36:22 +0200

    Del 2

--- insert.php
+++ insert.php
@@ -3,9 +3,19 @@ require "settings/init.php";
 
 if(!empty($_POST["data"])){
     $data = $_POST["data"];
+    $file = $_FILES;
 
-    $sql = "INSERT INTO film (filmNavn, filmBeskrivelse, filmPris) VALUES(:filmNavn, :filmBeskrivelse, :filmPris)";
-    $bind = [":filmNavn" => $data["filmNavn"], ":filmBeskrivelse" => $data["filmBeskrivelse"], ":filmPris" => $data["filmPris"]];
+    if(!empty($file["filmBillede"]["tmp_name"])){
+        move_uploaded_file($file["filmBillede"]["tmp_name"], "uploads/" . basename($file["filmBillede"]["name"]));
+    }
+
+    $sql = "INSERT INTO film (filmNavn, filmBeskrivelse, filmPris, filmBillede) VALUES(:filmNavn, :filmBeskrivelse, :filmPris, :filmBillede)";
+    $bind = [
+            ":filmNavn" => $data["filmNavn"],
+            ":filmBeskrivelse" => $data["filmBeskrivelse"],
+            ":filmPris" => $data["filmPris"],
+            ":filmBillede" => (!empty($file["filmBillede"]["tmp_name"])) ? $file["filmBillede"]["name"] : NULL,
+    ];
 
     $db->sql($sql, $bind, false);
 
@@ -37,22 +47,30 @@ if(!empty($_POST["data"])){
 
 <body>
 
-<form method="post" action="insert.php">
+<form method="post" action="insert.php" enctype="multipart/form-data">
     <div class="row">
+        <div class="col-12">
+            <label class="form-label" for="filmBillede">Film billede</label>
+            <input type="file" class="form-control" id="filmBillede" name="filmBillede">
+        </div>
+
         <div class="col-12 col-md-6">
             <label for="filmNavn">Film navn</label>
             <input class="form-control" type="text" name="data[filmNavn]" id="filmNavn" placeholder="Film navn" value="">
         </div>
+
         <div class="col-12 col-md-6">
             <label for="filmPris">Film pris</label>
             <input class="form-control" type="number" step="0.1" name="data[filmPris]" id="filmPris" placeholder="Film pris" value="">
         </div>
+
         <div class="col-12">
             <div class="form-group">
                 <label for="filmBeskrivelse">Film beskrivelse</label>
                 <textarea class="form-control" name="data[filmBeskrivelse]" id="filmBeskrivelse"></textarea>
             </div>
         </div>
+
         <div class="col-12 col-md-6 offset-md-6">
             <button class="form-control btn btn-primary" type="submit" id="btnSubmit">Tilføj Film</button>
         </div>
