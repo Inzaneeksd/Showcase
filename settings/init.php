2d869a48fd924ed878c2bef92c493bc22974bb1e
Inzaneeksd <17danielmoller@gmail.com>
2021-10-12 01:40:08 +0200

    Færdig

--- settings/init.php
+++ settings/init.php
@@ -3,16 +3,16 @@ require "classes/classDB.php";
 
 define("CONFIG_LIVE", "0"); // 0: Test enviroment || 1: Live enviroment
 
-if(CONFIG_LIVE == 0){
+if(CONFIG_LIVE == 1){
     $DB_SERVER = "localhost";
     $DB_NAME = "filmarkiv";
     $DB_USER = "root";
     $DB_PASS = "";
 }else{
-    $DB_SERVER = "";
-    $DB_NAME = "";
-    $DB_USER = "";
-    $DB_PASS = "";
+    $DB_SERVER = "danielmmoller.dk.mysql:3306";
+    $DB_NAME = "danielmmoller_dkfilm";
+    $DB_USER = "danielmmoller_dkfilm";
+    $DB_PASS = "SutDen";
 }
 
 $db = new db($DB_SERVER, $DB_NAME, $DB_USER, $DB_PASS);
